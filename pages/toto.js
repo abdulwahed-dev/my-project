c4790901472a5d167211a40c867d775843b50843
Abdulwahed Dev <abdulwahed.dev@gmail.com>
2022-03-25 05:56:55 +0200

    just red color

--- pages/toto.js
+++ pages/toto.js
@@ -66,7 +66,7 @@ export default function Toto() {
                         <span className="sm:ml-3">
                             <button
                                 type="button"
-                                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                             >
                                 <CheckIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                                 Publish
